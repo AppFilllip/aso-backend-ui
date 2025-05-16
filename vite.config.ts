7873f6decce5850a8790cb6fab21dda3f60cb330
AppFilllip <info@appfillip.com>
2025-05-16 16:33:06 +0530

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // Important: base must match your GitHub repo name!
-  base: '/aso-backend-ui/', // <-- CHANGE THIS if your repo name changes
-})
