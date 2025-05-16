244533e5e2d872e2c4051e942e9ef1d1e451ffbd
AppFilllip <info@appfillip.com>
2025-05-16 16:12:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/aso-backend-ui/", // IMPORTANT: Match your repo name!
-});
+  // Important: base must match your GitHub repo name!
+  base: '/aso-backend-ui/', // <-- CHANGE THIS if your repo name changes
+})
